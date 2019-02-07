84857906ed8123604052e79dd3dbefdeb9ec1bc9
world2enjoy <47141001+world2enjoy@users.noreply.github.com>
2019-02-07 16:50:47 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@
       <a href="index.php">Greet someone else</a>
     <? } else { ?>
       <form class="greetingForm" action="index.php" method="post">
-        <label for="who">Say hello to</label>
+        <label for="who">Say hello to whom</label>
         <input type="text" name="who">
         <input type="submit" name="greet" value="Say Hello">
       </form>
